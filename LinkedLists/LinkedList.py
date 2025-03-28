9f8d62d2ddcd998d269b195c8cac6387863be668
parisaAMahdavi <akhavanparisa90@gmail.com>
2025-03-28 11:27:36 -0400

    Added queue class using linked list

--- LinkedLists/LinkedList.py
+++ LinkedLists/LinkedList.py
@@ -51,6 +51,55 @@ class LinkedList:
         return self
 
 
+
+class Queue:
+    def __init__(self):
+        self.linkedList = LinkedList()
+    
+    def __str__(self):
+        values = [str(x) for x in self.linkedList]
+        return ' '.join(values)
+    
+    def enqueue(self, value):
+        newNode = Node(value)
+        if self.linkedList.head == None:
+            self.linkedList.head = newNode
+            self.linkedList.tail = newNode
+        else:
+            self.linkedList.tail.next = newNode
+            self.linkedList.tail = newNode
+    
+    def isEmpty(self):
+        if self.linkedList.head == None:
+            return True
+        else:
+            return False
+    
+    def dequeue(self):
+        if self.isEmpty():
+            return "There is not any node in the Queue"
+        else:
+            tempNode = self.linkedList.head
+            if self.linkedList.head == self.linkedList.tail:
+                self.linkedList.head = None
+                self.linkedList.tail = None
+            else:
+                self.linkedList.head = self.linkedList.head.next
+            return tempNode
+    
+    def peek(self):
+        if self.isEmpty():
+            return "There is not any node in the Queue"
+        else:
+            return self.linkedList.head
+    
+    def delete(self):
+        self.linkedList.head = None
+        self.linkedList.tail = None
+
+
+
+
 # linked_list = LinkedList()
 # linked_list.generate(10, 0, 99)
 # print(linked_list)
